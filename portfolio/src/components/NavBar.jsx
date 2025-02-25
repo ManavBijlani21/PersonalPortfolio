559fcce874518818eb3c89d7636150dc23902b98
ManavBijlani21 <119971901+ManavBijlani@users.noreply.github.com>
2025-02-26 04:37:33 +1030

    Integrated the SocialLinks component with App.js file

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 const NavBar = () => {
+
+    const [nav, setNav]  = useState(false);
 
    const links = [
     {
@@ -41,7 +43,27 @@ const NavBar = () => {
             </li>
         ))}
     </ul>
-  </div>);
+
+    <div onClick = {() => setNav(!nav)} className = "cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
+        {nav ? <FaTimes size = {30} /> : <FaBars size = {30} />}
+    </div>
+
+    {/* Only show the column list of tabs, if nav is true */}
+    {nav && (
+    <ul className = "flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+    {links.map(({ id, link }) => (
+            <li
+            key = {id}
+            className = "px-4 cursor-pointer capitalize py-6 text-4xl" >
+                {link}
+            </li>
+        ))}
+
+    </ul>
+    )}
+  </div>
+
+  );
 }
 
 export default NavBar
